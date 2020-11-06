abfe9695f768e98b200672d0e9cc586500753135
Oliver Thomas <63861649+oliverthomas93@users.noreply.github.com>
2020-11-06 13:08:21 +0000

    Nav changes

--- main.js
+++ main.js
@@ -15,9 +15,17 @@ $(document).ready(function() {
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav a');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+            link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
     });
 }
 navSlide();
